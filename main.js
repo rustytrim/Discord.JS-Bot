413c626102dab8048fdd6bb9b0c38575a2a1cc49
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-06-17 19:45:37 -0400

    Removed unneeded stuff

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const DiscordJS = require('discord.js');
-const fs = require('fs');
-const { token, prefix, owner } = require('./config.json');
+const { token } = require('./config.json');
 
 const client = new DiscordJS.Client();
 client.commands = new DiscordJS.Collection();

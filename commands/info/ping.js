599aa4b03a19d299899e418fcf84fa7e73b7e061
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-06-17 15:29:01 -0400

    Update ping.js

--- commands/info/ping.js
+++ commands/info/ping.js
@@ -1,21 +1,24 @@
 const { MessageEmbed } = require('discord.js');
+const { owner } = require('../../config.json');
 
 module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(client, message, args) {
-    let embed = new MessageEmbed()
-    .setTitle('Ping')
-    .setColor('RANDOM')
-    .setDescription("Ping is being calculated...")
+		let embed = new MessageEmbed()
+    		.setTitle('Ping')
+    		.setColor('RANDOM')
+    		.setDescription("Ping is being calculated...")
     
-    message.channel.send(embed).then(msg => {
-      const ping = msg.createdTimestamp - message.createdTimestamp;
-      let embed = new MessageEmbed()
-      .setTitle('Ping')
-      .setColor('RANDOM')
-      .setDescription(`Bot Latency: ${ping}\nAPI Latency: ${client.ws.ping}`)
-      msg.edit(embed);
-    });
-	},
+    		message.channel.send(embed).then(msg => {
+      		const ping = msg.createdTimestamp - message.createdTimestamp;
+      		let embed = new MessageEmbed()
+      		.setTitle('Ping')
+      		.setColor('RANDOM')
+      		.setDescription(`Bot Latency: ${ping}\nAPI Latency: ${client.ws.ping}`)
+      		.setFooter(`Bot hosted by ${owner.tag}. Bot coded by RT#0999`)
+
+      		msg.edit(embed);
+		});
+	}
 };

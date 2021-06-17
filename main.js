60e0154c336952baedffafbe9c72b47832f704fc
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-06-17 19:44:05 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,34 +3,9 @@ const fs = require('fs');
 const { token, prefix, owner } = require('./config.json');
 
 const client = new DiscordJS.Client();
-
 client.commands = new DiscordJS.Collection();
 
-const commandFolders = fs.readdirSync('./commands');
-
-for (const folder of commandFolders) {
-    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
-    for (const file of commandFiles) {
-        const cmd = require(`./commands/${folder}/${file}`);
-        client.commands.set(cmd.name, cmd);
-    }
-}
-
-client.on('ready', () => {
-  console.log(`${client.user.tag}, is online!`);
-});
-
-client.on('message', (message) => {
-  if(!message.content.startsWith(prefix) || message.author.bot) return;
-  const args = message.content.slice(prefix.length).trim().split(/ +/);
-  const cmd = args.shift().toLowerCase();
-  if(!client.commands.has(cmd)) return;
-  try {
-		client.commands.get(cmd).execute(client, message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply(`An error has occured. Please contact ${owner.tag} for assistance.`);
-	}
-})
+require('./utils/commandHandler.js')(client)
+require('./utils/eventHandler.js')(client)
 
 client.login(token);

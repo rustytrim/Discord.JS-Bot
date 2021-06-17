a2edfee1f3d4735ed38f77736f72e6112bb8c2a1
RT <46906497+rtgamingwdt@users.noreply.github.com>
2021-06-17 19:50:15 -0400

    Create message.js

---
+++ events/client/message.js
@@ -0,0 +1,17 @@
+const { prefix, owner } = require('../../config.json');
+
+module.exports = (client, message) => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const cmd = args.shift().toLowerCase();
+    
+    if(!client.commands.has(cmd)) return;
+    
+    try {
+		  client.commands.get(cmd).execute(client, message, args);
+	  } catch (error) {
+		  console.error(error);
+		  message.reply(`An error has occured. Please contact ${owner.tag} for assistance.`);
+    }
+}
